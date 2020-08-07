521d43d4515cc3c2f2ee9ec8514f040c584fced2
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2020-08-07 22:10:31 +0530

    Add global styles

---
+++ styles/global.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+    fontFamily: "opensans-regular",
+  },
+  appbar: {
+    justifyContent: "flex-start",
+    width: "100%",
+    backgroundColor: "#00005C",
+    top: 0,
+    right: 0,
+    left: 0,
+    shadowOpacity: 0,
+    shadowOffset: { height: 0 },
+    shadowRadius: 0,
+  },
+  // bottomNav: {
+  // backgroundColor: "#00005C",
+  // },
+});
